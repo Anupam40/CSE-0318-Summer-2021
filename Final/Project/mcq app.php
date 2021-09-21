af7007e8b29edaef4c51eb00ac9ef1159b03aabc
Anupam Ghosh <86396957+Anupam40@users.noreply.github.com>
2021-09-22 01:45:59 +0600

    Add files via upload

---
+++ Final/Project/mcq app.php
@@ -0,0 +1,67 @@
+<input type="radio" name="q1>text<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name ="viewport" content ="widh=divice-width, initial-scale=1.0">
+    <title>MCQ App</title>
+     <style>
+      h1{  
+         background-color:teal;
+         text-align:center;
+         color:white
+         padding: 10px;
+        font-size: 50px;  
+        
+  }
+  input[type="radio"]{
+    padding-right: 15px;
+}
+ body{
+    background-color:green;
+    
+ }
+div{
+    margin-left:300px;
+    background:left:300px;
+    background:left:green;
+    background-color:green;
+    
+    background:color:center;
+}
+
+
+p{
+  font-weight:bold;
+  font-size:25px;
+}
+  </style>
+ 
+</head>
+<body>
+<h1>Online MCQ Application</h1>  
+<div>
+<form action="mcq2.php" method="post"> 
+  <p>1. Which of the following HTML 4 attributes is removed in HTML5?</p>
+   A. <input type="radio" name="q1" value="text" required>text <br>
+   B. <input type="radio" name="q1" value="rules" required> rules <br>
+   C. <input type="radio" name="q1" value="link" required> link <br>
+   D. <input type="radio" name="q1" value="all of the mentioned" required> all of the mentioned <br> <br>
+  <P>2. Which of the following is not a newly added element in HTML5?</p>
+   A. <input type="radio" name="q2" value="aticle" required> aticle <br>
+   B. <input type="radio" name="q2" value="audio" required> audio <br>
+   C. <input type="radio" name="q2" value="nav" required> nav <br>
+   D. <input type="radio" name="q2" value="frameset" required> frameset <br> <br>
+   <p>3. Track and field star Carl Lewis won how many gold medals at the 1984 Olympic games?</p>
+   A. <input type="radio" name="q3" value="Two" required>Two <br>
+   B. <input type="radio" name="q3" value="Three" required>Three <br>
+   C. <input type="radio" name="q3" value="Four" required>Four <br>
+   D. <input type="radio" name="q3" value="Eight" required>Eight <br> <br>
+  
+   
+   <input type="submit" name="submit"value="submit">
+</form>
+</div> 
+
+
+</body>
+</html> 
